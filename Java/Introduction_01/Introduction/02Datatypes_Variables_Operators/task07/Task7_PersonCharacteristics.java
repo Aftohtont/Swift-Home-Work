67d0580ee330ab6c3b649445e34d68d11422da05
Aftohtont <35743667+Aftohtont@users.noreply.github.com>
2019-04-27 18:32:26 +0300

    Add files via upload

---
+++ Java/Introduction_01/Introduction/02Datatypes_Variables_Operators/task07/Task7_PersonCharacteristics.java
@@ -0,0 +1,19 @@
+package homework.dataTypes_Variables_02;
+
+public class Task7_PersonCharacteristics {
+    public static void main(String[] args) {
+        String fName="Georgi";
+        String lName="Georgiev";
+        byte age=12;
+        int year=2002;
+        float weight=48.3f;
+        float  tall=156f;
+        String proffesia="Student";
+
+        System.out.println(fName+" "+lName+" is "+age+" years old. His weight is "+weight+" and he is "+tall+ "cm tall. He is a student ");
+
+    }
+
+
+
+}
