dd6b2dbb8035607c7ecb580eb30e04365063bf32
Aftohtont <35743667+Aftohtont@users.noreply.github.com>
2019-04-27 15:43:34 +0300

    Add files via upload

---
+++ JavaBeginners/Introduction_01/HelloworldApplication.java
@@ -0,0 +1,18 @@
+package homework.Introduction_01;
+
+//define class content: 1) modifier 2) keyword-class 3) name of class 4) bracket of block {]- scope of class
+
+//1) modifier 2) keyword-class 3) name of class      4) bracket of block {here we add lines of code]
+       public      class       HelloworldApplication {                      //*1) define class
+
+    public static void main(String[] args){                                 //*2) define main method-start point of excecute
+
+        System.out.println("Hello, world!");                                //*3) content of method-write what method do
+
+    }
+}
+/* we has tree bullets here:
+        *1) define class
+        *2) define main method-start point of excecute
+        *3) content of method-write what method do
+ */
