07883e2ebbc24b25bb72aa61b9cf80feb23971b8
Aftohtont <35743667+Aftohtont@users.noreply.github.com>
2019-04-28 01:27:07 +0300

    Add files via upload

---
+++ Java/Introduction_01/Introduction/oopprinciplesextrafeature/task02/clients/CompanyClient.java
@@ -0,0 +1,7 @@
+package homework.oop_extrafeature_08.Таsk02_Bank.clients;
+
+public class CompanyClient extends Client {
+    public CompanyClient(String name) {
+        super(name);
+    }
+}
---
+++ Java/Introduction_01/Introduction/oopprinciplesextrafeature/task02/clients/IndividualCLient.java
@@ -0,0 +1,9 @@
+package homework.oop_extrafeature_08.Таsk02_Bank.clients;
+
+import homework.oop_extrafeature_08.Таsk02_Bank.clients.Client;
+
+public class IndividualCLient extends Client {
+    public IndividualCLient(String name) {
+        super(name);
+    }
+}
