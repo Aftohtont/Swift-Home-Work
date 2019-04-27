07ed0b4950ad1f4979bd7fdf4aee65c1714a8ee9
Aftohtont <35743667+Aftohtont@users.noreply.github.com>
2019-04-27 18:18:31 +0300

    Add files via upload

---
+++ Java/Introduction_01/Introduction/02Datatypes_Variables_Operators/task04/Task4_InchToCm.java
@@ -0,0 +1,17 @@
+package homework.dataTypes_Variables_02;
+
+public class Task4_InchToCm {
+
+    public static void main(String[] args) {
+        float cm=12;
+        float inch=2.54f;
+        float result=cm*inch;
+        System.out.println(result);
+        cm=23.23f;
+        result=(cm*inch);
+        System.out.println(result);
+
+    }
+
+
+}
